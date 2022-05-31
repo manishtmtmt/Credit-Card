aeff6ae6c802b14a1a64af23ffba4ce2a3d46845
manishtmtmt <manisht.mt.mt@gmail.com>
2022-05-31 17:29:43 +0530

    update ui

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,32 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import styles from "./styles.module.css";
 
 const Card = ({ data }) => {
   return (
-      <>
-        <Box>
-            <Text>{data.cardHolderName}</Text>
-            <Text>{data.cardNumber}</Text>
-            <Flex gap={2}>
-                <Text>{data.expiryMonth}</Text>
-                <Text>{data.expiryYear}</Text>
-                <Text>{data.cvv}</Text>
-            </Flex>
-        </Box>
-      </>
-  )
+    <>
+      <Box className={styles.card} p="5">
+        <Box><Text className={styles.visa}>VISA</Text></Box>
+        <Box><Image src="https://cdn-icons-png.flaticon.com/512/6404/6404078.png" className={styles.icon} /></Box>
+        <Text className={styles.name}>{data.cardHolderName}</Text>
+        <Text className={styles.cardno}>{data.cardNumber}</Text>
+        <Flex gap={5} className={styles.flex} p="2">
+          <Box>
+            <Text>Expiry Month</Text>
+            <Text>{data.expiryMonth}</Text>
+          </Box>
+          <Box>
+            <Text>Expiry Year</Text>
+            <Text>{data.expiryYear}</Text>
+          </Box>
+          <Box>
+            <Text>CVV</Text>
+            <Text>{data.cvv}</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
 };
 
 export default Card;
